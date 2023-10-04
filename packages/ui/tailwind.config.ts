b262e1ed6c7321a31d9721a065c3bd456bf696f9
Landry Carroll <landry@boldist.co>
2023-10-03 20:02:29 -0400

    core: create storybook app

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,11 +1,11 @@
-import { type Config } from "tailwindcss";
-import { config as snippetsPreset } from "@snippets/tailwind";
+import { type Config } from 'tailwindcss'
+import { config as snippetsPreset } from '@snippets/tailwind'
 
 export const config = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   presets: [snippetsPreset],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
