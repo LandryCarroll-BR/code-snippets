1a09ceaa7387eeef60695db23f28e9176b15cad1
Landry Carroll <landry@boldist.co>
2023-10-03 20:15:38 -0400

    core: add storybook styling addon

--- apps/workshop/src/stories/Button.stories.ts
+++ apps/workshop/src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button } from './Button'
+import { Button } from '@snippets/ui'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -25,26 +25,6 @@ type Story = StoryObj<typeof meta>
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
-  },
-}
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-}
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-}
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    children: 'Button',
   },
 }
